755db81fdb8c3b4227c631115c09f2481def041c
shahinmondal <52560393+shahinmondal@users.noreply.github.com>
2021-08-10 11:17:05 +0300

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,1 +1,52 @@
 
+
+var allahref=document.getElementsByTagName("a");
+for(var c=0;c<allahref.length;c++)
+{
+if(allahref[c].hostname!="www.allhostweb.blogspot.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="web.facebook.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="www.youtube.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="play.google.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="plus.google.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="www.linkedin.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="www.instagram.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="twitter.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="1.bp.blogspot.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="2.bp.blogspot.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="3.bp.blogspot.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="4.bp.blogspot.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="www.blogger.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="www.instagram.com" && allahref[c].hostname!="")
+{
+allahref[c].href="allhostweb.blogspot.com?&&url=_"+allahref[c].href;
+  }
+}
+
+
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({&#39;gtm.start&#39;:
+new Date().getTime(),event:&#39;gtm.js&#39;});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!=&#39;dataLayer&#39;?&#39;&amp;l=&#39;+l:&#39;&#39;;j.async=true;j.src=
+&#39;https://www.googletagmanager.com/gtm.js?id=&#39;+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,&#39;script&#39;,&#39;dataLayer&#39;,&#39;GTM-MWNXXRQ&#39;);</script>
+<!-- End Google Tag Manager -->     
+    <!-- Google Analytics -->
+
+(function(i,s,o,g,r,a,m){i[&#39;GoogleAnalyticsObject&#39;]=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,&#39;script&#39;,&#39;https://www.google-analytics.com/analytics.js&#39;,&#39;ga&#39;);
+ga(&#39;create&#39;, &#39;UA-202577850-1&#39;, &#39;auto&#39;);
+ga(&#39;send&#39;, &#39;pageview&#39;);
+
+
+/*
+-----------------------------------------------
+Blogger Template Style
+Name:     shahin
+Designer: shahin mondal
+URL:      www.allhostweb.blogspot.com
+Date:     11 Jul 2021
+-----------------------------------------------
+*/
+
+ (function(){var s = {};(function(){/*  Copyright The Closure Library Authors. SPDX-License-Identifier: Apache-2.0 */ var c={},f=this||self;var l=/#|$/;function n(d){var g=d.search(l),a;a:{for(a=0;0<=(a=d.indexOf("fmt",a))&&a<g;){var b=d.charCodeAt(a-1);if(38==b||63==b)if(b=d.charCodeAt(a+3),!b||61==b||38==b||35==b)break a;a+=4}a=-1}if(0>a)return null;b=d.indexOf("&",a);if(0>b||b>g)b=g;a+=4;return decodeURIComponent(d.substr(a,b-a).replace(/\+/g," "))};function r(d,g,a){function b(){--p;if(0>=p){var e;(e=d.GooglebQhCsO)||(e={});var q=e[g];q&&(delete e[g],(e=q[0])&&e.call&&e())}}for(var p=a.length+1,m=0;m<a.length;m++){var h=n(a[m]),k=null;1!=h&&2!=h||!(h=d.document.getElementById("goog_conv_iframe"))||h.src||(k=h);k||(k=new Image);k.onload=b;k.src=a[m]}b()}var t=["ss_"],u=s||f;t[0]in u||"undefined"==typeof u.execScript||u.execScript("var "+t[0]); for(var v;t.length&&(v=t.shift());)t.length||void 0===r?u[v]&&u[v]!==Object.prototype[v]?u=u[v]:u=u[v]={}:u[v]=r;}).call(this);;s.ss_(window,'OjE1NjQ1NjcyNjcwNTE',['https://www.google.com/pagead/1p-user-list/966957858/?random\x3d1564567267051\x26cv\x3d9\x26fst\x3d1564567200000\x26num\x3d1\x26bg\x3dffffff\x26guid\x3dON\x26u_h\x3d732\x26u_w\x3d412\x26u_ah\x3d732\x26u_aw\x3d412\x26u_cd\x3d24\x26u_his\x3d1\x26u_tz\x3d-420\x26u_java\x3dfalse\x26u_nplug\x3d0\x26u_nmime\x3d0\x26gtm\x3d2oa7o0\x26sendb\x3d1\x26data\x3devent%3Dgtag.config\x26frm\x3d0\x26url\x3dhttps%3A%2F%2Fwww.ics-dubai.com%2F\x26tiba\x3dTranslation%20Services%20provider%20Companies%20%2F%20Creative%20Agency%20in%20UAE%2C%20Kuwait%2C%20Bahrain%2C%20Saudi%20Arabia%2C%20Oman%20%E2%80%93%20ICS\x26async\x3d1\x26fmt\x3d3\x26is_vtc\x3d1\x26random\x3d3774635818\x26resp\x3dGooglemKTybQhCsO\x26rmt_tld\x3d0\x26ipr\x3dy','https://www.google.com.kw/pagead/1p-user-list/966957858/?random\x3d1564567267051\x26cv\x3d9\x26fst\x3d1564567200000\x26num\x3d1\x26bg\x3dffffff\x26guid\x3dON\x26u_h\x3d732\x26u_w\x3d412\x26u_ah\x3d732\x26u_aw\x3d412\x26u_cd\x3d24\x26u_his\x3d1\x26u_tz\x3d-420\x26u_java\x3dfalse\x26u_nplug\x3d0\x26u_nmime\x3d0\x26gtm\x3d2oa7o0\x26sendb\x3d1\x26data\x3devent%3Dgtag.config\x26frm\x3d0\x26url\x3dhttps%3A%2F%2Fwww.ics-dubai.com%2F\x26tiba\x3dTranslation%20Services%20provider%20Companies%20%2F%20Creative%20Agency%20in%20UAE%2C%20Kuwait%2C%20Bahrain%2C%20Saudi%20Arabia%2C%20Oman%20%E2%80%93%20ICS\x26async\x3d1\x26fmt\x3d3\x26is_vtc\x3d1\x26random\x3d3774635818\x26resp\x3dGooglemKTybQhCsO\x26rmt_tld\x3d1\x26ipr\x3dy']);})();
